ec132518901ab901699827461f333e571cf45275
varela77 <90734107+varela77@users.noreply.github.com>
2021-11-08 22:01:50 -0600

    Add files via upload

---
+++ Ventas/Form1.cs
@@ -0,0 +1,27 @@
+﻿//Elvin Noé Palma Hernández     20192001535
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ventas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Conserva tus sueños, nunca sabes cuando te harán falta");
+        }
+    }
+}
